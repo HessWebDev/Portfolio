c07a3dd1fd9fa8215364c8be2808042634308f05
Riley Hess <HessWebDev@gmail.com>
2022-03-10 19:19:28 -0600

    created most of layout for portfolio

---
+++ JavaScript/functions.js
@@ -0,0 +1,21 @@
+
+    /*let skills = document.getElementsByClassName("SkillsImage");
+
+    let l = window.screen.width;
+    let w = 1300;
+    var margin = 0;
+
+    function Moveskills(){
+    
+        if (margin == w){
+            margin = 0 + "px";
+
+        }
+        else{
+            skills.style.marginLeft = margin + "px";
+        }
+
+        margin += 10;
+    }
+
+    Moveskills(); */
